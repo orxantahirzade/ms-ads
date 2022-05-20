f1afbb7c07c59fb77dd262463eed1f9153dca5e6
orxantahirzade <orxan.tahirzada@gmail.com>
2022-05-20 20:53:41 +0400

    Initial commit

---
+++ src/main/java/az/iktlab/msads/MsAdsApplication.java
@@ -0,0 +1,13 @@
+package az.iktlab.msads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAdsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/iktlab/msads/MsAdsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.iktlab.msads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
